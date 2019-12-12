800109e2b520a6296545523949e3d560b1d24384
Antonio Hernandez <antonio@mlmusings.com>
2019-12-12 12:07:34 -0600

    Cleaning up, adding esag files as jplg counter measure

--- shell_scripts/download_files.sh
+++ shell_scripts/download_files.sh
@@ -3,12 +3,14 @@
 #time aria2c -d ionex_files/ -i ionex_files_urls.txt -j 120 --c true
 
 START_TIME=$SECONDS
+URL_FILE=../ionex_urls_esag.txt
+DOWNLOAD_LOCATION=../ionex_esag
 
 # Download and save the files listed in the ionex_files_urls.txt located in the parent directory to a new directory named ionex_files.
-aria2c --dir=../ionex_files/ --input-file=../ionex_files_urls.txt --max-concurrent-downloads=120 --continue=true 
+aria2c --dir=$DOWNLOAD_LOCATION --input-file=$URL_FILE --max-concurrent-downloads=120 --continue=true 
 
 ELAPSED_TIME=$(($SECONDS - $START_TIME))
 
-TOTAL_DOWNLOADED_FILES=$(wc -l ../ionex_files_urls.txt) 
+TOTAL_DOWNLOADED_FILES=$((wc -l $DOWNLOAD_LOCATION))
 
 echo "$TOTAL_DOWNLOADED_FILES were downloaded in $(($ELAPSED_TIME/60)) minutes $(($ELAPSED_TIME%60)) seconds."
