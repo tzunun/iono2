b3f47e42637b5b78e17321afc47f04a1b53268e4
Antonio Hernandez <antonio@mlmusings.com>
2020-07-02 09:32:24 -0500

    Edited description, and made the download shell script more robust.  It should now work from any directory and download files to the expected location

--- shell_scripts/download_files.sh
+++ shell_scripts/download_files.sh
@@ -3,17 +3,26 @@
 #time aria2c -d ionex_files/ -i ionex_files_urls.txt -j 120 --c true
 
 #Create directory to store the downloaded files
-mkdir ../ionex_esag
+DIR=$(dirname $(dirname $(readlink -f "$0")))
+ESAG_DOWNLOAD_DIR="${DIR}/ionex_esag" 
 
-START_TIME=$SECONDS
-URL_FILE=../ionex_urls_esag.txt
-DOWNLOAD_LOCATION=../ionex_esag
+if [  -d "$ESAG_DOWNLOAD_DIR" ]; then
+				echo "$ESAG_DOWNLOAD_DIR exists. Starting download.."
+else
+				echo "Directory not present, creating directory $ESAG_DOWNLOAD_DIR"
+				mkdir ${ESAG_DOWNLOAD_DIR}
+fi
 
-# Download and save the files listed in the ionex_files_urls.txt located in the parent directory to a new directory named ionex_files.
-aria2c --dir=$DOWNLOAD_LOCATION --input-file=$URL_FILE --max-concurrent-downloads=120 --continue=true 
+START_TIME=`date +%s`
+URL_FILE="${DIR}/ionex_urls_esag.txt"
+DOWNLOAD_LOCATION="${ESAG_DOWNLOAD_DIR}"
 
-ELAPSED_TIME=$(($SECONDS - $START_TIME))
+# Download and save the files listed in the ionex_urls_esag.txt which is in
+# the parent directory to a new directory named ionex_esag.
+aria2c --dir=$DOWNLOAD_LOCATION --input-file=$URL_FILE --max-concurrent-downloads=120 --continue=true
 
-TOTAL_DOWNLOADED_FILES=$((wc -l $DOWNLOAD_LOCATION))
+END_TIME=`date +%s`
+ELAPSED_TIME=$((END_TIME-START_TIME))
 
-echo "$TOTAL_DOWNLOADED_FILES were downloaded in $(($ELAPSED_TIME/60)) minutes $(($ELAPSED_TIME%60)) seconds."
+TOTAL_DOWNLOADED_FILES=$((ls -l $DOWNLOAD_LOCATION | wc -l))
+echo "$TOTAL_DOWNLOADED_FILES were downloaded in $ELAPSED_TIME"
